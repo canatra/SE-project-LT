05063f0ecf46bd776088652052ce8bfee0046c9d
canatra <joy.bushnell@gmail.com>
2017-03-20 13:20:09 -0400

    updated some things
    added strip, still having issues with out of bounds/range for tokens list, if statements not printing out correctly for translation of if statements

--- translator.py
+++ translator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 #function to print the tabs at the beginning of the lines
-def printtabs(f, i):
+def printtabs(fo, i):
 	if i < 1:
 		return
 	else:
@@ -11,7 +11,7 @@ def printtabs(f, i):
 
 #variables; fi should probably be set as a command line argument
 index = 0;
-fo = open("program.py", w)
+fo = open("program.py", 'w')
 
 #getting the line and splitting it into tokens
 
@@ -19,82 +19,83 @@ with open("program.f90",'rb') as fi:
 	while True:
         	codeline=fi.readline()
 		if not codeline: break
-
+                codeline =codeline.strip(" ")
                 tokens = codeline.split(" ")
-                                
+                print tokens
 	        #if statement controls
                 
 		if codeline == "\n":
 			fo.write("\n")
 
 		elif (tokens[0] == "IF") or (tokens[0] == "if"):
-			printtabs(f, index)
+			printtabs(fo, index)
 			fo.write("if")
 			counter = 1
 			while (codeline[counter] != "THEN") and (codeline[counter] != "then") and (codeline[counter] != "EXIT") and (codeline[counter] != "exit"):
-				fo.write(" " , codeline[counter])
+				fo.write(" " + codeline[counter])
 				counter = counter + 1
 			if (codeline[counter] == "THEN") or (codeline[counter] == "then"):
 				fo.write(":\n")
 			elif (codeline[counter] == "EXIT") or (codeline[counter] == "exit"):
 				fo.write(": break\n")
-			index = index + 1
+			        index = index + 1
 		elif (tokens[0] == "EXIT") or (tokens[0] == "exit"):
 			fo.write("break\n")
 		elif (tokens[0] == "CONTINUE") or (tokens[0] == "continue"):
 			fo.write("continue\n")
 		elif (tokens[0] == "ELSE") or (tokens[0] == "else"):
 			index = index - 1
-			printtabs(f, index)
+			printtabs(fo, index)
 			fo.write("elif")
 			counter = 2
 			while (codeline[counter] != "THEN") and (codeline[counter] != "then") and (codeline[counter] != "EXIT") and (codeline[counter] != "exit"):
-				fo.write(" " , codeline[counter])
+				fo.write(" " + codeline[counter])
 				counter = counter + 1
-			fo.write(":\n")
-			index = index + 1
+			        fo.write(":\n")
+			        index = index + 1
 		elif (tokens[0] == "ENDIF") or (tokens[0] == "endif"):
 			index = index - 1
-			printtabs(f, index)
+			printtabs(fo, index)
 			fo.write(":\n")
-		#do loop controls
+		        #do loop controls
 		elif (tokens[0] == "DO") or (tokens[0] == "do"):
-			printtabs(f, index)
-			fo.write("for ", tokens[2], " in range(", tokens[4], tokens[5], ":")
+			printtabs(fo, index)
+			fo.write("for "+ tokens[2] + " in range(" + tokens[4]+ tokens[5] + ":")
 			index = index + 1
 		elif (tokens[0] == "ENDDO") or (tokens[0] == "enddo"):
 			index = index - 1
-			printtabs(f, index)
+			printtabs(fo, index)
 			fo.write("\n")
 		elif (tokens[0] == "END") or (tokens[0] == "end"):
 			#some loops use "end for" or "end do" rather than run together
 			index = index - 1
-			printtabs(f, index)
+			printtabs(fo, index)
 			fo.write("\n")	
                 elif (tokens[0] == "select") or (tokens[0] == "SELECT"):
-                        selector = (tokens[2].split('(')).split(')')[0]
-                       
+                        selector = tokens[2]#.split('(')).split(')')[0]
+                        print selector
                         
                         while tokens[0] != "end" or tokens[0] != "END":
                                 codeline = fi.readline()
+                                codeline =codeline.strip(" ")
                                 tokens = codeline.split(" ")
                                 
 		                if codeline == "\n":
 			                fo.write("\n")
 
-                                elif "default" in tokens or "DEFAULT" in tokens:
+                                elif tokens[0] == "default" or tokens[0] == "DEFAULT":
                                         fo.write("else: \n")
-                                        index += 1
+                                        index += 2
                                         
                                 elif tokens[0] == "case" or tokens[0] == "CASE":
                                         case = (tokens[1].split('('))[1].split(')')[0]
-                                        index += 1
-                                        fo.write("if ", case," :\n")
+                                        index += 2
+                                        fo.write("if "+selector + " = " + case +" :\n")
                                         #check for ranges
                                 else:
-                                        printtabs(f, index)
+                                        printtabs(fo, index)
                                         index -= 1
-                                        fo.write(token[0])
+                                        fo.write(tokens[0])
 
                                         #need to check statement
                                         #if it calls a function
