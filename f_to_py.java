c926044a64c1979eb11c237ef73a0d0a5c41fcb7
canatra <joy.bushnell@gmail.com>
2017-04-26 18:24:01 -0400

    Add files via upload

--- f_to_py.java
+++ f_to_py.java
@@ -14,24 +14,37 @@ public class f_to_py extends fortran77BaseListener{
     public void enterProgram(fortran77Parser.ProgramContext ctx)
     {
 	//open the python file, write program comments in the python file, translate libraries?
-	System.out.println("entering program");
+	
+	//System.out.println("Entering program");
+	
+	
     }
     
     @Override
     public void exitProgram(fortran77Parser.ProgramContext ctx){
 
+	
     }
 
 
-    @Override public void enterFunctionSubprogram(fortran77Parser.FunctionSubprogramContext ctx) { }
+    @Override public void enterFunctionSubprogram(fortran77Parser.FunctionSubprogramContext ctx) {
 
-    @Override public void exitFunctionSubprogram(fortran77Parser.FunctionSubprogramContext ctx) { }
+	//input def function name ():
+	// tab counter += 1
+    }
+
+    @Override public void exitFunctionSubprogram(fortran77Parser.FunctionSubprogramContext ctx) {
+	//tab counter -= 1
+    }
 
     @Override public void enterSubroutineSubprogram(fortran77Parser.SubroutineSubprogramContext ctx) { }
 
     @Override public void exitSubroutineSubprogram(fortran77Parser.SubroutineSubprogramContext ctx) { }
 
-    	@Override public void enterFunctionStatement(fortran77Parser.FunctionStatementContext ctx) { }
+    	@Override public void enterFunctionStatement(fortran77Parser.FunctionStatementContext ctx) {
+
+	    //increment tab counter
+	}
 
     
     	@Override public void exitFunctionStatement(fortran77Parser.FunctionStatementContext ctx) { }
@@ -39,7 +52,70 @@ public class f_to_py extends fortran77BaseListener{
     	@Override public void enterSubroutineStatement(fortran77Parser.SubroutineStatementContext ctx) { }
 
     	@Override public void exitSubroutineStatement(fortran77Parser.SubroutineStatementContext ctx) { }
+    
+    
+    	@Override public void enterPrintStatement(fortran77Parser.PrintStatementContext ctx) {
+
+	    
+	    System.out.print("print ");
+	    String delims ="print";
+	    String tobeparsed = new String(ctx.getText());
+	    String [] tokens = tobeparsed.split(delims);
+	    
+	    
+	    for (int i = 1; i < tokens.length; ++i)
+		{
+		    
+		    if (tokens[i].equals("*") )
+		    	continue;
+		    else{
+		    	System.out.print(tokens[i]);
+		    	
+				    }
+		}
+	    System.out.println(" ");   
+	}
+
+    //use this function and parse the read statement;
+	@Override public void enterReadStatement(fortran77Parser.ReadStatementContext ctx) {
+	    String delims ="[read,,]+";
+	    String tobeparsed = new String(ctx.getText());
+	    String [] tokens = tobeparsed.split(delims);
+	    
+	    
+	    for (int i = 1; i < tokens.length; ++i)
+		{
+		    
+		    if (tokens[i].equals("*") )
+		    	continue;
+		    else{
+		    	System.out.print(tokens[i]);
+		    	System.out.println(" = input(\" \")");
+		    	    }
+		}
+	}
+    
+
+@Override public void enterVarRef(fortran77Parser.VarRefContext ctx){
+    // System.out.println("in enterVarRef function");
+    //System.out.print(ctx.getText());
+
+}
+
+
+    //don't use these functions
+   @Override public void enterFormatIdentifier(fortran77Parser.FormatIdentifierContext ctx) {
+
+       
+       //System.out.println(ctx.getText());
+    }
+
+    @Override public void enterIoList(fortran77Parser.IoListContext ctx) {// System.out.println(ctx.getText());
+    }
+	@Override public void enterAssignmentStatement(fortran77Parser.AssignmentStatementContext ctx) {
+	    System.out.println(ctx.getText());
+	    
+	}
 
     
-    
-}
+}//end of class
